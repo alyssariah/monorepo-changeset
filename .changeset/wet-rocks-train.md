c8e3785a9c03c9e1767326900c3b1cc8c61c3783
Alyssa Jackson <alyssariah@gmail.com>
2025-05-14 16:27:50 -0400

    commit changeset

---
+++ .changeset/wet-rocks-train.md
@@ -0,0 +1,5 @@
+---
+"package-a": minor
+---
+
+A second console log
--- packages/package-a/index.js
+++ packages/package-a/index.js
@@ -2,4 +2,5 @@
 module.exports = () => {
     console.log("Hello from Package A!");
     console.log("New Change in Package A");
+    console.log("Another change");
 };
