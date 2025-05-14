8348a86dcc1ba90a58bb3b505250568d2c297121
Alyssa Jackson <alyssariah@gmail.com>
2025-05-14 16:44:37 -0400

    new push

--- packages/package-a/index.js
+++ packages/package-a/index.js
@@ -1,5 +1,6 @@
 // packages/package-a/index.js
 module.exports = () => {
     console.log("Hello from Package A!");
+    console.log("New Change in Package A");
     console.log("Another change");
 };
