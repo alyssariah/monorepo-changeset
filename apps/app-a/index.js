0f4a1b78cdc7231ada8f23c6687ec5e2aafe8312
Alyssa Jackson <alyssariah@gmail.com>
2025-06-02 17:47:49 -0400

    chore: clean up app a

--- apps/app-a/index.js
+++ apps/app-a/index.js
@@ -1,7 +1,3 @@
 module.exports = () => {
     console.log("Hello from App A!");
-    console.log("New Change in App A");
-    console.log('New Feature in App A');
-    console.log('New fix in App A')
-    console.log("New chore in App A")
 };
